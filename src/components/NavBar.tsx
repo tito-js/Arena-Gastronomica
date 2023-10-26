8b5d22b20fbeaab3f0fa49749a2177f84f0d0f40
Gabriel Rocha <118408324+tito-js@users.noreply.github.com>
2023-10-26 14:10:23 -0300

    merge branch 'Back'

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,13 @@ import Menu from "./Menu";
 import Link from "next/link";
 import CartIcon from "./CartIcon";
 import Image from "next/image";
+import UserLinks from "./UserLinks";
 
 const Navbar = () => {
   const user = false;
   return (
     <div className="h-12 text-blue-500 p-4 flex items-center justify-between border-b-2 border-b-green-500 uppercase md:h-24 lg:px-20 xl:px-40">
-      {/* LEFT LINKS */}
+      {/* ESQUERDA LINKS */}
       <div className="hidden md:flex gap-4 flex-1">
         <Link href="/">Início</Link>
         <Link href="/menu">Menu</Link>
@@ -16,23 +17,19 @@ const Navbar = () => {
       </div>
       {/* LOGO */}
       <div className="text-xl md:font-bold flex-1 md:text-center">
-        <Link href="/">Arena Gastronômica</Link>
+        <Link href="/">Arena Gastronomica</Link>
       </div>
       {/* MOBILE MENU */}
       <div className="md:hidden">
         <Menu />
       </div>
-      {/* RIGHT LINKS */}
+      {/* DIREITA LINKS */}
       <div className="hidden md:flex gap-4 items-center justify-end flex-1">
-        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-yellow-400 px-1 rounded-md">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
           <Image src="/phone.png" alt="" width={20} height={20} />
           <span>(16)99286-9585</span>
         </div>
-        {!user ? (
-          <Link href="/login">Login</Link>
-        ) : (
-          <Link href="/orders">Pedidos</Link>
-        )}
+        <UserLinks/>
         <CartIcon />
       </div>
     </div>
